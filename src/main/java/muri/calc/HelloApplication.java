823724405f165e1e1478cb8e08388e9c11ffcafe
Murilo <murilo_no@outlook.com>
2024-10-09 15:22:34 -0300

    icone do app placeholder

--- src/main/java/muri/calc/HelloApplication.java
+++ src/main/java/muri/calc/HelloApplication.java
@@ -3,6 +3,7 @@ package muri.calc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculadora-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("calculadora.css")).toExternalForm());
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("icon.png"))));
         stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
